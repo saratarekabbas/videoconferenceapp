824946f75361af2ba6ecea9a52d535aba6ceb322
Sara Tarek <saraatarekk@icloud.com>
2021-06-20 23:55:37 +0200

    Added the Rest Controller file. NOTE, API IS STILL NOT WORKING/NOT INTEGRATED INTO WEBSITE

--- RESTAPI/RestController.php
+++ RESTAPI/RestController.php
@@ -1,1 +1,39 @@
 <!-- Controller -->
+<!-- Capability of API to request data without having access to the database -->
+
+<?php
+require_once("RESTAPI/UserRestHandler.php");
+
+$view = "";
+if(isset($_GET["view"])){
+    $view = $_GET["view"];
+}
+
+/*
+controls the RESTful services
+URL mapping
+*/
+
+switch($view){
+
+	case "all":
+
+		// to handle REST Url /user/list/
+         $userRestHandler = new UserRestHandler(); //create an obj of the handler
+         $userRestHandler->getAllUsers(); //point to func getAllUsers()
+
+		break;
+
+	case "single":
+		//to handle REST Url /user/show/<id>/
+		$userRestHandler = new UserRestHandler();
+		$userRestHandler->getUser($_GET["id"]); //point to func getUser($id) in UserRestHandler()
+		break;
+
+	case "" :
+		//404 - not found;
+		break;
+}
+
+
+?>
